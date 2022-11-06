547286a4aba92cbb999e5003baaed60af2478d6b
Hevidk72 <71612634+Hevidk72@users.noreply.github.com>
2022-11-06 13:39:43 +0100

    Changed app color to Amber

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,21 @@ import 'HomePage.dart';
 void main() {
   runApp(const MyApp());
 }
-  
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  
+
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {    
+  Widget build(BuildContext context) {
     return MaterialApp(
-      // in the below line, we are specifying title of our app. 
-      title: 'A Clean World',      
-      // in the below line, we are hiding our debug banner. 
+      // in the below line, we are specifying title of our app.
+      title: 'A Clean World',
+      // in the below line, we are hiding our debug banner.
       debugShowCheckedModeBanner: false,
-      // in the below line, we are specifying theme. 
+      // in the below line, we are specifying theme.
       theme: ThemeData(
-        primarySwatch: Colors.blue,        
+        primarySwatch: Colors.amber,
       ),
       // First Screen of our App
       home: HomePage(),
