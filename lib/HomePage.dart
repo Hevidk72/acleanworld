52d109b750828b9cf801addccb2608fdfb479989
Henrik Fuglsig Villadsen <hevi@curit.dk>
2022-11-13 00:09:06 +0100

    Get current location on start

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -18,45 +18,46 @@ class _HomePageState extends State<HomePage> {
   bool _serviceEnabled = false;
   double curLati = 55.6577728;
   double curLong = 9.399007;
+  final mapController = MapController();
 
   bool bDebugMode = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // in the below line, we are specifying our app bar.
-        appBar: AppBar(
-          // setting background color for app bar
-          backgroundColor: Colors.blueAccent,
-          // setting title for app bar.
-          title: const Text("A Cleaner World"),
-          centerTitle:true,
-          leading: const Icon(Icons.menu_book),
-        ),
-        body: Container(
-            // in the below line, creating google maps.
-            child: FlutterMap(
-                options: MapOptions(
-                    center: LatLng(curLati, curLong), zoom: 17.0),
-                layers: [
-              TileLayerOptions(
-                  urlTemplate:
-                      'https://tile.openstreetmap.org/{z}/{x}/{y}.png'),
-              MarkerLayerOptions(markers: [
-                Marker(
-                    width: 30.0,
-                    height: 30.0,
-                    point: LatLng(curLati, curLong),
-                    builder: (ctx) => Container(
-                            child: Container(
-                          child: Icon(
-                            Icons.location_on,
-                            color: Colors.blueAccent,
-                            size: 40,
-                          ),
-                        )))
-              ])
-            ])));
+      // in the below line, we are specifying our app bar.
+      appBar: AppBar(
+        // setting background color for app bar
+        backgroundColor: Colors.blueAccent,
+        // setting title for app bar.
+        title: const Text("A Cleaner World"),
+        centerTitle: true,
+        leading: const Icon(Icons.menu),
+      ),
+      body: Container(
+          // in the below line, creating google maps.
+          child: FlutterMap(
+              mapController: mapController,
+              options: MapOptions(center: LatLng(curLati, curLong), zoom: 17.0),
+              layers: [
+            TileLayerOptions(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'),
+            MarkerLayerOptions(markers: [
+              Marker(
+                  width: 30.0,
+                  height: 30.0,
+                  point: LatLng(curLati, curLong),
+                  builder: (ctx) => Container(
+                          child: Container(
+                        child: Icon(
+                          Icons.location_on,
+                          color: Colors.blueAccent,
+                          size: 40,
+                        ),
+                      )))
+            ])
+          ])),
+    );
   }
 
   void initLocation() async {
@@ -65,6 +66,10 @@ class _HomePageState extends State<HomePage> {
       _serviceEnabled = await location.requestService();
 
       if (!_serviceEnabled) {
+        print("_locationData.latitude=" +
+            _locationData.latitude.toString() +
+            " _locationData.longitude=" +
+            _locationData.longitude.toString());
         return;
       }
     }
@@ -72,47 +77,62 @@ class _HomePageState extends State<HomePage> {
     if (_permissionGranted == PermissionStatus.denied) {
       _permissionGranted = await location.requestPermission();
       if (_permissionGranted != PermissionStatus.granted) {
+        print("_locationData.latitude=" +
+            _locationData.latitude.toString() +
+            " _locationData.longitude=" +
+            _locationData.longitude.toString());
         return;
       }
     }
     _locationData = await location.getLocation();
     curLati = _locationData.latitude!;
     curLong = _locationData.longitude!;
+
     print("_locationData.latitude=" +
         _locationData.latitude.toString() +
         " _locationData.longitude=" +
         _locationData.longitude.toString());
   }
 
-@override
+  @override
   void initState() {
-    initLocation();
-    showAlert(context);    
-    super.initState();    
+    super.initState();
+   
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      // Use `MapController` as needed
+      initLocation();
+      mapController.move(LatLng(curLati,curLong), 17.0);
+    });
+    // after map move 
+    print("_locationData.latitude=" +
+        _locationData.latitude.toString() +
+        " _locationData.longitude=" +
+        _locationData.longitude.toString());
+    showAlert(context);
+    
   }
 
- Future showAlert(BuildContext context) async {
- 
+  Future showAlert(BuildContext context) async {
     await Future.delayed(Duration(seconds: 6));
- 
+
     showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: new Text("Welcome To Our App :) Latitude="+curLati.toString()+" Longitude="+curLong.toString()),
-        actions: <Widget>[
-          OutlinedButton(
-            child: new Text("OK"),                                    
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
-      );
-     },
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: new Text("Welcome To Our App :) Latitude=" +
+              curLati.toString() +
+              " Longitude=" +
+              curLong.toString()),
+          actions: <Widget>[
+            OutlinedButton(
+              child: new Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
-
-
-
 }
