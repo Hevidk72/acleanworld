a4bc838851f43bb7150e70c60d07001101037bd2
Hevidk72 <71612634+Hevidk72@users.noreply.github.com>
2022-11-21 01:22:47 +0100

    added currentTrip List array

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -8,11 +8,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:acleanworld/widgets/drawer.dart';
 import 'package:acleanworld/utils/utils.dart';
 
-
 Future<void> main() async {
-  print("Supabase.initialize");
-  await Supabase.initialize(url: 'https://zbqoritnaqhkridbyaxc.supabase.co', anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpicW9yaXRuYXFoa3JpZGJ5YXhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njg2MzI0NDEsImV4cCI6MTk4NDIwODQ0MX0.NO3SvLCPEmXMFIVFiHBYV9ZLp0o2IFgndMzpkwQG_F0');
-  runApp(const HomePage());
+    runApp(const HomePage());
 }
 
 class HomePage extends StatefulWidget {
@@ -31,16 +28,20 @@ class _HomePageState extends State<HomePage> {
   String? _serviceError = '';
   int interActiveFlags = InteractiveFlag.all;
   final Location _locationService = Location();
-  //final supabase = Supabase.instance.client;
-
+  bool debug = true;
+  List<trip> currentTrip = [];
+  
   @override
   void initState() {
     super.initState();
     _mapController = MapController();
     initLocationService();
-    //showAlert(context, "Ready to Rock and Roll!", 0);
+    
+    if (debug) debugPrint("Supabase.initialize");
+    Supabase.initialize(url: 'https://zbqoritnaqhkridbyaxc.supabase.co', anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpicW9yaXRuYXFoa3JpZGJ5YXhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njg2MzI0NDEsImV4cCI6MTk4NDIwODQ0MX0.NO3SvLCPEmXMFIVFiHBYV9ZLp0o2IFgndMzpkwQG_F0');
+    final supabase = Supabase.instance.client;
+        //showAlert(context, "Ready to Rock and Roll!", 0);
   }
-
 
    void initLocationService() async 
    {
@@ -68,8 +69,11 @@ class _HomePageState extends State<HomePage> {
                 // If Live Update is enabled, move map center
                 if (_liveUpdate) 
                 {
-                  // showAlert(context, "Ready to Rock and Roll! \n ${_currentLocation!.latitude!}", 0);
+                   //showAlert(context, "Ready to Rock and Roll! \n ${_currentLocation!.latitude!}", 0);
+                   print("Logging position ");
                   _mapController.move(LatLng(_currentLocation!.latitude!, _currentLocation!.longitude!), _mapController.zoom);
+                  // Todo log trip data here:
+                  currentTrip.add(trip(lat: _currentLocation!.latitude!, long: _currentLocation!.longitude!));
                 }
               });
             }
@@ -90,10 +94,12 @@ class _HomePageState extends State<HomePage> {
       if (e.code == 'PERMISSION_DENIED') 
       {
         _serviceError = e.message;
+        showAlert(context, _serviceError!, 1);
       } 
       else if (e.code == 'SERVICE_STATUS_ERROR') 
       {
         _serviceError = e.message;
+        showAlert(context, _serviceError!, 1);
       }
       location = null;
     }
@@ -123,7 +129,7 @@ class _HomePageState extends State<HomePage> {
         width: 80,
         height: 80,
         point: currentLatLng,
-        builder: (ctx) => Icon(Icons.my_location),
+        builder: (ctx) => const Icon(Icons.my_location),
       ),
     ];
 
@@ -140,9 +146,9 @@ class _HomePageState extends State<HomePage> {
           [
             Padding
             (
-              padding: const EdgeInsets.only(top: 0, bottom: 0),
+              padding: const EdgeInsets.only(top: 0, bottom: 8),
               child: _serviceError!.isEmpty
-                  ? Text('Din position: (${currentLatLng.latitude}, ${currentLatLng.longitude}) and Zoom=${_currentZoom}' )//Text('This is a map that is showing (${currentLatLng.latitude}, ${currentLatLng.longitude}) and zoom=${_mapController.zoom}.')
+                  ? Text('Din position: (${currentLatLng.latitude}, ${currentLatLng.longitude}) and Zoom=$_currentZoom' )//Text('This is a map that is showing (${currentLatLng.latitude}, ${currentLatLng.longitude}) and zoom=${_mapController.zoom}.')
                   : Text('Error occured while acquiring location. Error Message : $_serviceError'),
             ),
             Flexible
@@ -180,14 +186,28 @@ class _HomePageState extends State<HomePage> {
               _liveUpdate = !_liveUpdate;
               if (_liveUpdate) 
               {
+                if (debug)  debugPrint("Start Button pressed");
                 _mapController.move(LatLng(currentLatLng.latitude, currentLatLng.longitude), 18.49);
                 interActiveFlags = InteractiveFlag.rotate | InteractiveFlag.pinchZoom | InteractiveFlag.doubleTapZoom;
+                // Todo call dialog box to start recording trip in to an array
+
                 
                 ScaffoldMessenger.of(context).showSnackBar(const SnackBar( content: Text('I live update mode virker kun zoom og rotation.'),));
               } 
               else 
               {
+                if (debug)  debugPrint("Stop Button pressed");
+                _mapController.move(LatLng(currentLatLng.latitude, currentLatLng.longitude), 18.49);
                 interActiveFlags = InteractiveFlag.all;
+                // Todo call dialog box to stop recording current trip or cancel trip. Get notes and kg litter collected and update Database.
+                if (debug)  
+                {
+                  for (var trip in currentTrip) 
+                  {                    
+                      debugPrint("lat is: ${trip.lat} and long is: ${trip.long}");                    
+                  }
+                  
+                }
               }
             });
           },
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,17 +1,65 @@
 import 'package:flutter/material.dart';
 
+class trip
+{
+   double lat;
+   double long;
+   trip({required this.lat, required this.long});
+}
   Future showAlert(BuildContext context, String messageText, int delayed) async {
     await Future.delayed(Duration(seconds: delayed));
 
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        return AlertDialog(
-          
-          title: new Text(messageText),
+        return AlertDialog(          
+          title: Text(messageText),
           actions: <Widget>[
             OutlinedButton(
-              child: new Text("OK"),
+              child: const Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
+  Future showNewTripDialog(BuildContext context, String messageText, int delayed) async {
+    await Future.delayed(Duration(seconds: delayed));
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(          
+          title: Text(messageText),
+          actions: <Widget>[
+            OutlinedButton(
+              child: const Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+Future showEndTripDialog(BuildContext context, String messageText, int delayed) async {
+    await Future.delayed(Duration(seconds: delayed));
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(          
+          title: Text(messageText),
+          actions: <Widget>[
+            OutlinedButton(
+              child: const Text("OK"),
               onPressed: () {
                 Navigator.of(context).pop();
               },
